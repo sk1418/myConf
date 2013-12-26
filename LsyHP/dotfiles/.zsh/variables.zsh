73c1fb22a453e408df1239d8c71f32e750d559e6
Kai Yuan <kent.yuan@Gmail.com>
2013-12-26 17:25:20 +0100

    add zsh prompt

---
+++ LsyHP/dotfiles/.zsh/variables.zsh
@@ -0,0 +1,28 @@
+export ORACLE_HOME=/usr/lib/oracle/xe/app/oracle/product/10.2.0/server
+export ORACLE_SID=XE
+export JAVA_HOME=/opt/java
+export DEV_HOME=/home/kent/javaEnv
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ORACLE_HOME/lib:$DEV_HOME/sapjco-linuxintel-2.1.8
+#export M2_HOME=$DEV_HOME/maven
+export M2_HOME=$DEV_HOME/maven3
+export MAVEN_HOME=$DEV_HOME/maven3
+export MAVEN_OPTS="-Djavax.net.ssl.trustStore=/home/kent/javaEnv/7p/7p-keystore -Djavax.net.ssl.trustStorePassword=changeit"
+export JBOSS_HOME=$DEV_HOME/jboss-eap-5.1.2/jboss-eap-5.1/jboss-as
+export TOMCAT_HOME=$DEV_HOME/tomcat5
+export PATH=$PATH:/home/kent/javaEnv/maven3/bin:$ORACLE_HOME/bin:/home/kent/javaEnv/ant/bin:$HOME/bin
+#export TERM=xterm-256color
+
+#proxy setting
+export http_proxy=http://localhost:8888
+export https_proxy=https://localhost:8888
+export ftp_proxy=https://localhost:8888
+export no_proxy="localhost, 127.0.0.1, localdomain.com"
+
+export HTTP_PROXY=http://localhost:8888
+export HTTPS_PROXY=https://localhost:8888
+export FTP_PROXY=https://localhost:8888
+export NO_PROXY="localhost, 127.0.0.1, localdomain.com, 192.168.0.*, *.dlh.de, 57.*.*.*"
+
+#only for EC project
+export ANT_OPTS=-Dfile.encoding=iso-8859-1
+export _JAVA_OPTIONS="-Dawt.useSystemAAFontSettings=on -Dswing.aatext=true"
