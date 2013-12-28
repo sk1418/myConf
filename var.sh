7996932db5be7ba1c93272de33661c4c41bee1f0
Kai Yuan <kent.yuan@Gmail.com>
2013-12-28 18:40:24 +0100

    add modeline to scripts

--- var.sh
+++ var.sh
@@ -1,4 +1,8 @@
-
+#========================================
+# common variable and functions for 
+# getConf and applyConf scripts
+#========================================
+test
 #get current path
 script_path="${BASH_SOURCE[0]}"
 if ([ -L "${script_path}" ]) then
